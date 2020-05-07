7a2b0d823209ec31c08a70a951ce5fb52f47cfaa
Eduardo Muñoz <eduardo@gestalabs.com>
2020-05-07 10:24:20 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class platform360 extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.greetingBox}>
+        <Text style={styles.greeting}>
+          Welcome to React 360
+        </Text>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('platform360', () => platform360);
