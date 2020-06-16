681163ffe01ed8089e13064ceadaa63aba8b0221
Eduardo Muñoz <eduardo@gestalabs.com>
2020-06-16 10:25:41 -0500

    Código React, registro de los componentes que componen la Plataforma

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+/**
+ * Contiene código React, registro los componentes que viven en la Plataforma
+ **/
 
 import { AppRegistry } from 'react-360';
 
@@ -9,8 +11,10 @@ import Controls from './components/Controls';
 import * as Store from './utils/Store';
 import seed from './utils/seed';
 
+// Store del App (utils/Store.js)
 Store.initialize(seed);
 
+// Registro de los Componentes
 AppRegistry.registerComponent('Catalog', () => Catalog);
 AppRegistry.registerComponent('CurrentMovie', () => CurrentMovie);
 AppRegistry.registerComponent('Controls', () => Controls);
